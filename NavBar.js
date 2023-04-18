4eb89b5d4faf82bbe62c7e51c1935c5d7f85c8a7
RapidKS <sharma.kaush120375@gmail.com>
2023-04-18 16:31:50 +0530

    HomePage

---
+++ NavBar.js
@@ -0,0 +1,4 @@
+<picture>
+    <source srcset="https://ak1.ostkcdn.com/img/mxc/04062023-HP-A1-INTL-V2-1400x550_DSK-INTL.svg" media="(min-width: 768px)">
+    <img class="first-secondry-img" src="https://ak1.ostkcdn.com/img/mxc/04062023-HP-A1-INTL-V2-767x800_MBL-INTL.svg" alt="Spring Black Friday">
+</picture>
