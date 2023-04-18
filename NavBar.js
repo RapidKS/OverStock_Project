be160ca97c04261d0de36a43d3698a4b008d7ca2
RapidKS <sharma.kaush120375@gmail.com>
2023-04-18 17:01:38 +0530

    sticky head

--- NavBar.js
+++ NavBar.js
@@ -1,4 +1,13 @@
-<picture>
-    <source srcset="https://ak1.ostkcdn.com/img/mxc/04062023-HP-A1-INTL-V2-1400x550_DSK-INTL.svg" media="(min-width: 768px)">
-    <img class="first-secondry-img" src="https://ak1.ostkcdn.com/img/mxc/04062023-HP-A1-INTL-V2-767x800_MBL-INTL.svg" alt="Spring Black Friday">
-</picture>
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
